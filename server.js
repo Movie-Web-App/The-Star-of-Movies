150261f3cb63776a80a90a6592ad4b494c39a472
anasswalha95 <anas.swalha@yahoo.com>
2021-03-09 22:55:12 +0200

    pulling test branch

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const superagent = require("superagent");
 const layout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 4800;
 const server = express();
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+const secret = process.env.secret;
+const saltRounds = 10;
 
 const override = require("method-override");
 // const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
@@ -22,6 +27,8 @@ server.get("/", home);
 server.get("/details", detailHandler);
 server.get("/search", searchHandler);
 server.get("/databaseinit", databaseinit);
+server.get("/signup", signup);
+server.get("/signin", signin);
 
 var movies_ids = [];
 let flag = true;
@@ -44,7 +51,7 @@ function databaseinit(req, res) {
           .then((results) => {
             // console.log(results);
             if (results.rowCount == 0) {
-      
+
               let url2 = `https://imdb-api.com/en/API/Title/${key}/${item}/FullActor,FullCast,Posters,Images,Trailer,Ratings,Wikipedia`;
               superagent.get(url2).then((results) => {
                 let result = results.body;
@@ -105,8 +112,8 @@ function Movie(moviesData) {
 function capitalizeTheFirstLetterOfEachWord(words) {
   var separateWord = words.toLowerCase().split(' ');
   for (var i = 0; i < separateWord.length; i++) {
-     separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
-     separateWord[i].substring(1);
+    separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+      separateWord[i].substring(1);
   }
   return separateWord.join(' ');
 }
@@ -124,66 +131,66 @@ function home(req, res) {
 }
 
 function searchHandler(req, res) {
-  let searchedMov=[]
+  let searchedMov = []
   const key = process.env.IMDB_KEY1;
-  let title =null
+  let title = null
   if (req.query.search)
-  title= capitalizeTheFirstLetterOfEachWord(req.query.search);
-  else 
-  res.redirect("/")
+    title = capitalizeTheFirstLetterOfEachWord(req.query.search);
+  else
+    res.redirect("/")
   let SQL = `SELECT * FROM movies WHERE title LIKE '%${title}%' ;`
 
   client.query(SQL)
-  .then(data =>{
-   
-    if (data.rowCount==0){
+    .then(data => {
 
-     let URL=`https://imdb-api.com/en/API/Search/${key}/${title}`  
-      superagent.get(URL).then((results) => {
-        results.body.results.map(item=>{
-        let url2 = `https://imdb-api.com/en/API/Title/${key}/${item.id}/FullActor,FullCast,Posters,Images,Trailer,Ratings,Wikipedia`;
-              superagent.get(url2).then((results) => {
-                let result = results.body;
-                let actors = result.actorList.reduce((acc, cur) => {
-                  return (acc += cur.name + " , ");
-                }, "");
-                let SQL = `INSERT INTO movies(imdb_id,title,year,image,stars,runtime,genre,actors,plot,trailer,imDb_rate,metacritic_rate,theMovieDb_rate,rottenTomatoes_rate) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) returning * `;
-                let safeValues = [
-                  result.id,
-                  result.title,
-                  result.year,
-                  result.image,
-                  result.stars,
-                  result.runtimeMins,
-                  result.genres,
-                  actors,
-                  result.plot,
-                  result.trailer.linkEmbed,
-                  result.ratings.imDb,
-                  result.ratings.metacritic,
-                  result.ratings.theMovieDb,
-                  result.ratings.rottenTomatoes,
-                ];
-                client.query(SQL, safeValues).then((returnedData)=>{
-                  searchedMov.push(returnedData)
-                });
-              
+      if (data.rowCount == 0) {
+
+        let URL = `https://imdb-api.com/en/API/Search/${key}/${title}`
+        superagent.get(URL).then((results) => {
+          results.body.results.map(item => {
+            let url2 = `https://imdb-api.com/en/API/Title/${key}/${item.id}/FullActor,FullCast,Posters,Images,Trailer,Ratings,Wikipedia`;
+            superagent.get(url2).then((results) => {
+              let result = results.body;
+              let actors = result.actorList.reduce((acc, cur) => {
+                return (acc += cur.name + " , ");
+              }, "");
+              let SQL = `INSERT INTO movies(imdb_id,title,year,image,stars,runtime,genre,actors,plot,trailer,imDb_rate,metacritic_rate,theMovieDb_rate,rottenTomatoes_rate) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) returning * `;
+              let safeValues = [
+                result.id,
+                result.title,
+                result.year,
+                result.image,
+                result.stars,
+                result.runtimeMins,
+                result.genres,
+                actors,
+                result.plot,
+                result.trailer.linkEmbed,
+                result.ratings.imDb,
+                result.ratings.metacritic,
+                result.ratings.theMovieDb,
+                result.ratings.rottenTomatoes,
+              ];
+              client.query(SQL, safeValues).then((returnedData) => {
+                searchedMov.push(returnedData)
               });
-            })    
 
-      }).then(()=>{
-        console.log(searchedMov);
-        res.render("pages/moviespage", { movies: searchedMov });
-      })
-      .catch((error)=>{
-        res.json(error)
-      });
-     }else {
-     // res.json(result)
-     res.render("pages/moviespage", { movies: data.rows });
+            });
+          })
 
-     }
-  }) 
+        }).then(() => {
+          console.log(searchedMov);
+          res.render("pages/moviespage", { movies: searchedMov });
+        })
+          .catch((error) => {
+            res.json(error)
+          });
+      } else {
+        // res.json(result)
+        res.render("pages/moviespage", { movies: data.rows });
+
+      }
+    })
 }
 
 function detailHandler(req, res) {
@@ -192,6 +199,75 @@ function detailHandler(req, res) {
   client.query(SQL).then((results) => {
     res.render("pages/moviedetails", { movie: results.rows });
   });
+}
+
+function cryptPassword(password) {
+
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+
+    return hash
+
+  })
+}
+
+function isValidEmail(email) {
+
+  return /\S+@\S+\.\S+/.test(email);
+}
+
+function generateToken(id) {
+  const token = jwt.sign({
+    userId: id
+  },
+    process.env.SECRET, { expiresIn: '7d' }
+  );
+  return token;
+}
+
+function verifyToken(req, res, next) {
+  const token = req.headers['x-access-token'];
+  if (!token) {
+    return res.status(400).send({ 'message': 'Token is not provided' });
+  }
+  try {
+    const decoded = await jwt.verify(token, process.env.SECRET);
+    const text = 'SELECT * FROM users WHERE id = $1';
+    const { rows } = await db.query(text, [decoded.userId]);
+    if (!rows[0]) {
+      return res.status(400).send({ 'message': 'The token you provided is invalid' });
+    }
+    req.user = { id: decoded.userId };
+    next();
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+}
+
+function signup(req, res, next) {
+  let { username, useremail, password } = req.params
+
+  let checkForUserExistanceQuery = `select * from users where useremail=$1`
+  let safevals = [useremail]
+  client.query(checkForUserExistanceQuery, safevals).then((result) => {
+    if (result.rowCount == 0) {
+      password = cryptPassword(password)
+      let query = "insert into users(username, useremail,password) values($1,$2,$3)"
+      let safe = [username, useremail, password]
+      client.query(query, safe).then((result) => {
+        res.redirect("/login")
+      })
+    }
+    else {
+      res.render("pages/signup")
+    }
+
+  })
+
+}
+
+function signin(req, res, next) {
+
 }
 
 client.connect().then(() => {
